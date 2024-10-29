6973b7c1f807ce0c26c205d1e4588f85065e3fa1
Hoang Thien Phu Le <22521099@gm.uit.edu.vn>
2024-10-29 23:25:08 +0700

    Update ChessLogic

---
+++ ChessLogic/Player.cs
@@ -0,0 +1,37 @@
+﻿namespace ChessLogic
+{
+    public enum Player
+    {
+        None,
+        White,
+        Black
+    }
+    public static class PlayerExtensions
+    {  
+        public static Player Opponent(this Player player)
+        {
+ /* 
+This below code in the comment works fine but if you're using a recent version of .net (6.0 or above), you might see 
+'Convert switch statement to expression' option and could choose it by clickling 'ctrl + .' or 'alt + enter':
+
+            switch (player)
+            {
+                case Player.White:
+                    return Player.Black;
+                case Player.Black:
+                    return Player.White;
+                default:
+                    return Player.None;
+            }
+
+*/
+            return player switch
+            {
+                Player.White => Player.Black,
+                Player.Black => Player.White,
+                _ => Player.None,
+            };
+        }
+    }
+}
+
---
+++ ChessLogic/Position.cs
@@ -0,0 +1,61 @@
+﻿namespace ChessLogic
+{
+    //This class represents a position or square on the chess board:
+    public class Position
+    {
+        //The class needs two properties, one for the row, and another one for the column:
+        public int Row { get; }
+        public int Column { get; }
+
+        //Adding a Constructor which takes a row and a column:
+        public Position(int row, int column)
+        {
+            // Storing row and column in the properties:
+            Row = row;
+            Column = column;
+        }
+
+        //Adding a square color method:
+        public Player SquareColor()
+        {
+            if ((Row + Column) % 2 == 0)
+            {
+                return Player.White;
+            }
+            //Otherwise, if the sum is odd, then the squre is black:
+            return Player.Black;
+        }
+
+/*
+Equals and GetHashCode need to be overwritten so that the Position class can be used as the key in a dictionary. 
+Instead of doing so manually, Visual Studio can be used to generate these methods by creating an empty line, pressing 'Ctrl .', and clicking 'Generate Equals and GetHashCode...'. 
+Ensure that both row and column are selected, then check 'Generate operators'.
+*/
+        public override bool Equals(object obj)
+        {
+            return obj is Position position &&
+                   Row == position.Row &&
+                   Column == position.Column;
+        }
+        
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Column);
+        }
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+
+        public static Position operator +(Position pos, Direction dir)
+        {
+            return new Position(pos.Row + dir.RowDelta, pos.Column + dir.ColumnDelta);  
+        }
+    }
+}
