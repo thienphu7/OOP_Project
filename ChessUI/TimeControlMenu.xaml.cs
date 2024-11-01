d2276dd32a4cf62ae8ad9864bb27a2f93612bcbb
Hoang Thien Phu Le <22521099@gm.uit.edu.vn>
2024-11-01 21:54:28 +0700

    Add UI for the Timekeeper use case

---
+++ ChessUI/TimeControlMenu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ChessLogic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessUI
+{
+    /// <summary>
+    /// Interaction logic for TimeControlMenu.xaml
+    /// </summary>
+    public partial class TimeControlMenu : UserControl
+    {
+        public event Action<TimeMode> TimeModeSelected;
+        public TimeControlMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Standard_Click(object sender, RoutedEventArgs e)
+        {
+            TimeModeSelected?.Invoke(TimeMode.Standard);
+        }
+
+        private void Rapid_Click(object sender, RoutedEventArgs e)
+        {
+            TimeModeSelected?.Invoke(TimeMode.Rapid);
+        }
+
+        private void Blitz_Click(object sender, RoutedEventArgs e)
+        {
+            TimeModeSelected?.Invoke(TimeMode.Blitz);
+        }       
+    }
+}
