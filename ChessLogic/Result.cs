ade9fa63e67b7f25cf9f6a30c1dfb833b49c14c6
Hoang Thien Phu Le <22521099@gm.uit.edu.vn>
2024-11-01 22:23:12 +0700

    Update Result.cs for the Timekeeper use case

--- ChessLogic/Result.cs
+++ ChessLogic/Result.cs
@@ -1,21 +1,24 @@
-﻿namespace ChessLogic
+namespace ChessLogic
 {
     public class Result
     {
         public Player Winner { get; }
         public EndReason Reason { get; }
 
+        // Constructor to initialize Result with Winner and Reason
         public Result(Player winner, EndReason reason)
         {
             Winner = winner;
             Reason = reason;
         }
 
-        public static Result Win(Player winner)
+        // Factory method for a winning Result with default Checkmate end reason
+        public static Result Win(Player winner, EndReason endReason = EndReason.Checkmate)
         {
-            return new Result(winner, EndReason.Checkmate);
+            return new Result(winner, endReason);
         }
 
+        // Factory method for a draw Result with a specified reason
         public static Result Draw(EndReason reason)
         {
             return new Result(Player.None, reason);
